5e719ccb506d4d0e86dddc93dfc626222b19224f
codepipe <47555726+codepipe@users.noreply.github.com>
2021-12-05 11:57:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-echo "2021 aws nov batch chitra , snehal , raghu, sagar";
+echo "2021 aws nov batch chitra , snehal , raghu, sagar, now snehal will approve the code befoere the deploy into the server";
 ?>
 
 </body>
